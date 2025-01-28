f04d31277f9598325dd9b18903cc3e10f0c9bc30
TS <102474982+taesunyeom@users.noreply.github.com>
2025-01-28 16:22:52 +0900

    minor

--- _make_favicon.sh
+++ _make_favicon.sh
@@ -1,1 +1,1 @@
-convert -background transparent "images/circle_bw_crop.jpg" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+magick -background transparent "images/android-chrome-512x512.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
